04cb1fb334e521c2baff38c1748adada685a9abb
InfinityKing21 <39119163+InfinityKing21@users.noreply.github.com>
2023-05-01 03:47:49 +0530

    Add Different balloons
    Add a balloon class to ease adding more balloons
    Balloons now have score, speed, height and width associated with it

--- Main/Balloon.py
+++ Main/Balloon.py
@@ -13,35 +13,61 @@ width, height = 1280, 720
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Balloon Pop")
 
-# Set FPS and Clock
-fps = 60
-clock = pygame.time.Clock()
-
 # Set Webcam Dimensions
 cap = cv2.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
 
 # Load Balloon Image
-imgBalloon = pygame.image.load('../Resources/BalloonRed.png').convert_alpha()
-imgBackground = pygame.image.load('../Resources/Project - GUI/background.png').convert()
-rectBalloon = imgBalloon.get_rect()
-rectBalloon.x, rectBalloon.y = 500, 300
+preferredHeight = 150
+preferredWidth = 115
+heightToWidth =  preferredHeight / preferredWidth
+
+class Balloon:
+    def __init__(self, property, rawImage, height, width, points, defaultSpeed):
+        self.property = property
+        self.height = height
+        self.width = width
+        self.rawImage = rawImage
+        self.image = pygame.transform.scale(rawImage, (width, height))
+        self.rect = self.image.get_rect()
+        self.points = points
+        self.rect.x = 500
+        self.rect.y = 300
+        self.speed = defaultSpeed
+
+    def randomizeLocation(self, background):
+        self.rect.x = random.randint(100, background.shape[1] - 100)
+        self.rect.y = background.shape[0] + 50
+
+redBalloon = Balloon('red', pygame.image.load('../Resources/BalloonRed.png').convert_alpha(), 150, 115, 10, 11)
+goldBalloon = Balloon('gold', pygame.image.load('../Resources/BalloonGold.png').convert_alpha(), 160, 60, 20, 14)
+pinkBalloon = Balloon('pink', pygame.image.load('../Resources/BalloonPink.png').convert_alpha(), 175, 80, 5, 8)
+starBalloon = Balloon('star', pygame.image.load('../Resources/BalloonStar.png').convert_alpha(), 50, 50, 50, 20)
+
+balloonList = [redBalloon, goldBalloon, pinkBalloon, starBalloon]
+
+def randomBalloon(balloonList):
+    length = len(balloonList)
+    randomIndex = random.randint(0, length - 1)
+
+    # Reduce the occurence of star balloon
+    if balloonList[randomIndex].property == 'star':
+        secondSpin = random.randint(0, length - 1)
+        return balloonList[secondSpin]
+
+    return balloonList[randomIndex]
 
 # Initialize Variables
-speed = 11
+streamSpeed = 1
 score = 0
 name = ""
 
 # Initialize Hand Detector
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 
-# Function to Reset Balloon Position
-def resetBalloon():
-    rectBalloon.x = random.randint(100, img.shape[1] - 100)
-    rectBalloon.y = img.shape[0] + 50
-
 # Show Start Page
+imgBackground = pygame.image.load('../Resources/Project - GUI/background.png').convert()
 showStartPage = True
 while showStartPage:
     # Get Events
@@ -79,6 +105,12 @@ while showStartPage:
 
 startTime = time.time()
 totalTime = 60
+fps = 120
+clock = pygame.time.Clock()
+
+# Choose random Balloon
+balloon = randomBalloon(balloonList)
+
 # Main Game Loop
 while True:
     # Get Events
@@ -86,6 +118,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+
 
     # Apply Logic
     timeRemain = int(totalTime - (time.time() - startTime))
@@ -104,25 +137,27 @@ while True:
         success, img = cap.read()
         img = cv2.flip(img, 1)
         hands, img = detector.findHands(img, flipType=False)
-        rectBalloon.y -= speed
+        balloon.rect.y -= balloon.speed + streamSpeed
         # check if ballon has reached top without pop
-        if rectBalloon.y <0:
-            resetBalloon()
-            speed += 0.5
+        if balloon.rect.y <0:
+            balloon = randomBalloon(balloonList)
+            balloon.randomizeLocation(img)
+            streamSpeed += 0.5
 
         if hands:
             hand = hands[0]
             x, y, z = hand['lmList'][8]
-            if rectBalloon.collidepoint(x, y):
-                resetBalloon()
-                score += 10
-                speed += 1
+            if balloon.rect.collidepoint(x, y):
+                score += balloon.points
+                streamSpeed += 1
+                balloon = randomBalloon(balloonList)
+                balloon.randomizeLocation(img)
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         imgRGB = np.rot90(imgRGB)
         frame = pygame.surfarray.make_surface(imgRGB).convert()
         frame = pygame.transform.flip(frame, True, False)
         window.blit(frame, (0, 0))
-        window.blit(imgBalloon, rectBalloon)
+        window.blit(balloon.image, balloon.rect)
 
         font = pygame.font.Font('../Resources/Marcellus-Regular.ttf', 50)
         textScore = font.render(f'Score: {score}', True, (50, 50, 255))
